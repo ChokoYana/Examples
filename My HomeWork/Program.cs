579bef5ef108a4d0e91c73ccd456b2b8ac792c2f
Yana <yana.melnik2022@mail@mail.ru>
2022-08-20 02:09:55 +0300

    Task 4

---
+++ My HomeWork/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Imput number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+int min = number2;
+
+if(number1>number2)
+{
+    Console.WriteLine($"Maximum number is {number1} and minimum number is {number2}");
+}
+else
+{
+    Console.WriteLine($"Maximum number is {number2} and minimum number is {number1}");
+}
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.Write("Imput number1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput number3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int Maximum = number1;
+ 
+ if(Maximum>number2)
+ {
+    Console.WriteLine($"Maximum number is {number1}");
+ }
+else
+{
+    Maximum = number2;
+}
+if(Maximum>number3)
+{
+    Console.WriteLine($"Maximum number is {number2}");
+}
+else
+{
+    Maximum = number3;
+}
+Console.WriteLine($"Maximum number is {Maximum}");
+*/
