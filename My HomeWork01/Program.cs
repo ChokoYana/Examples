e4d6342088a60eee4b8daf6457e8b02364a12f8f
Yana <yana.melnik2022@mail@mail.ru>
2022-08-23 23:23:21 +0300

    Added task 2 and 3

--- My HomeWork01/Program.cs
+++ My HomeWork01/Program.cs
@@ -32,14 +32,26 @@ Console.WriteLine($"The second number {number} is {secondNum}");
 
 // Задача 13: Напишите программу, которая выводит третью 
 // цифру заданного числа или сообщает, что третьей цифры нет.
+
 /*
 void FindThirdNumber(int num)
 
 {
-    int num3 = num % 10;
-    if(num3 < 10)
-        Console.WriteLine($"The third number {num} of {num3}");
-    else Console.WriteLine($"The third number {num} not found");
+    if(num >= 100)
+    {
+        int current = num;
+        int thirdNum = 0;
+        while (current > 1000)
+        {
+            current = current / 10;
+        }
+        thirdNum = current % 10;
+        Console.WriteLine($"The third digit {num} is {thirdNum}");
+    }
+    else
+    {
+        Console.WriteLine($"Number {num} dont have third digit");
+    }
 }
 
 Console.Write("imput number: ");
@@ -50,4 +62,16 @@ FindThirdNumber(number);
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, 
 // обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+void SearchDayOff (int day)
+{
+    if (day < 6)
+        Console.WriteLine($"Day {day} is work day");
+    else Console.WriteLine($"Day {day} is day off");
+}
 
+Console.Write("imput day number(from 1 and to 7): ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+SearchDayOff(number);
+*/
