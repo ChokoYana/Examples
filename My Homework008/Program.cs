7b3add741e06d2fc68ff10e35560d1ca9703000a
Yana <yana.melnik2022@mail@mail.ru>
2022-09-14 22:13:13 +0300

    HW8 task 1,2,3

---
+++ My Homework008/Program.cs
@@ -0,0 +1,201 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по
+// убыванию элементы каждой строки двумерного массива.
+
+/*
+int[,] CreateRandom2dArray(int row, int column, int minValue, int maxValue)
+{
+    int[,] array = new int[row, column];
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void BubbleSort(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            for(int k = j + 1; k < array.GetLength(1); k++)
+                if(array[i, j] > array[i, k])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temp;
+                }
+
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(newArray);
+BubbleSort(newArray); 
+Show2dArray(newArray); */
+
+
+
+// Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+/*
+int[,] CreateRandom2dArray(int row, int column, int minValue, int maxValue)
+{
+    int[,] array = new int[row, column];
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void MinSumRows (int[,] array)
+{
+    int iRows = 0;
+    int sumRows = 2147483647;
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        int sumMin = 0;
+        
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            sumMin += array[i, j];
+        }
+        Console.WriteLine(sumMin);
+        
+        if(sumMin < sumRows) 
+        {
+            sumRows = sumMin;
+            iRows = i;
+        }
+    }
+    Console.WriteLine(iRows);
+}
+
+
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] newArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(newArray);
+MinSumRows(newArray);
+*/
+
+
+
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+/*
+int[,] CreateRandom2dArray(int row, int column, int minValue, int maxValue)
+{
+    int[,] array = new int[row, column];
+    for(int i = 0; i < row; i++)
+        for(int j = 0; j < column; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MatrixCalculation (int[,] array1, int[,] array2)
+{
+    int[,] arrayCalc = new int[array1.GetLength(0), array1.GetLength(1)];
+    for(int i = 0; i < array1.GetLength(0); i++)
+        for(int j = 0; j < array1.GetLength(1); j++)
+            for(int k = 0; k < array1.GetLength(1); k++)
+            {
+                arrayCalc[i,j] += array1[i,k] * array2[k,j];
+            }
+
+    return arrayCalc;
+}
+
+
+
+Console.WriteLine("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input the max possible value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+int[,] array1 = CreateRandom2dArray(m, n, min, max);
+Show2dArray(array1);
+int[,] array2 = CreateRandom2dArray(m, n, min, max);
+Show2dArray(array2);
+int[,] array3 = MatrixCalculation(array1, array2);
+Show2dArray(array3);
+*/
+
+
+// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу,
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+
+// Напишите программу, которая заполнит спирально массив 4 на 4.
